e0a5eee87d554dc6ac102d10a8f12df0d480f532
KOREAVC <KOREAVC@DESKTOP-OO4UAMB>
2023-09-04 18:30:46 +0900

    수정

--- p0904_성적폼/src/p0904/J0904_11.java
+++ p0904_성적폼/src/p0904/J0904_11.java
@@ -3,26 +3,21 @@ package p0904;
 public class J0904_11 {
 
 	public static void main(String[] args) {
-		
+
 		// 1부터 100까지의 합 : 5050
-		//1부터 100까지의 합에서 합이 200을 넘는 때는 언제일까요? i와 sum을 출력하시오.
-		
-		
+		// 1부터 100까지의 합에서 합이 200을 넘는 때는 언제일까요? i와 sum을 출력하시오.
+
 		// 1. 1부터 100까지 합을 구함.
 		// 2. 조건문을 사용해서 sum 200이상일때를 구함.
-		int i=0,sum=0;
-		for(i=1;i<=100;i++) {
-			sum = sum + i;
+		int i = 0, sum = 0;
+		for (i = 1; i <= 100; i++) {
+			sum += i;
+			if(sum >= 200) {
+				break;
+			}
 		}
-		System.out.println("합계 : "+sum);
-		if(sum<=200) {
-	      
-		}
-		
-		
-		
-		
-		
+		System.out.println("합계 : " + sum);
+
 //		// 1부터 100까지의 합을 출력하시오.
 //		int i=0;
 //		int sum=0;
@@ -31,10 +26,7 @@ public class J0904_11 {
 //		}
 //		
 //		System.out.printf("1부터 %d 까지 합계 : %d \n ",i-1,sum);
-		
-		
-		
-		
+
 //		int i=0;
 //		int sum=0;
 //		for(i=1;i<=10;i++) {
@@ -46,6 +38,6 @@ public class J0904_11 {
 //		System.out.println("for문 밖 sum : "+sum);
 //		
 
-	}//main
+	}// main
 
-}//class
+}// class
